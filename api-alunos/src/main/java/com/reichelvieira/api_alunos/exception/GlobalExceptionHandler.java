7a9c58f759a96c093afb185bd29858b76eca18a6
Eduardo Reichel Vieira <146209138+eduardoRvieira@users.noreply.github.com>
2026-09-21 21:48:22 -0300

    faz

--- api-alunos/src/main/java/com/reichelvieira/api_alunos/exception/GlobalExceptionHandler.java
+++ api-alunos/src/main/java/com/reichelvieira/api_alunos/exception/GlobalExceptionHandler.java
@@ -1,21 +1,22 @@
 package com.reichelvieira.api_alunos.exception;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.Instant;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler{
 
     @ExceptionHandler(AlunoNaoEncontradoException.class)
-    public ResponseEntity<String> handleAlunoNaoEncontradoException(AlunoNaoEncontradoException ex){
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+    public ResponseEntity<ErroResponse> handleAlunoNaoEncontradoException(AlunoNaoEncontradoException ex){
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ErroResponse(404, ex.getMessage(), Instant.now()));
     }
 
     @ExceptionHandler(EmailJaCadastradoException.class)
-    public ResponseEntity<String> handleEmailJaCadastradoException(EmailJaCadastradoException ex){
-        return ResponseEntity.status(HttpStatus.CONFLICT).body(ex.getMessage());
+    public ResponseEntity<ErroResponse> handleEmailJaCadastradoException(EmailJaCadastradoException ex){
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(new ErroResponse(409, ex.getMessage(), Instant.now()));
     }
 }
